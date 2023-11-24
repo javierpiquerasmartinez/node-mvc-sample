2d87863efbe8b304ef0e089463aa34628397ccc9
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-11-24 13:28:21 +0100

    Dependency injection

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 import express, { json } from 'express'
-import { moviesRouter } from './routes/movies.js'
+import { createMovieRouter } from './routes/movies.js'
 import { corsMiddleware } from './middlewares/cors.js'
 
-const app = express()
-app.use(json())
-app.use(corsMiddleware())
-app.disable('x-powered-by')
+export const createApp = ({ movieModel }) => {
+  const app = express()
+  app.use(json())
+  app.use(corsMiddleware())
+  app.disable('x-powered-by')
 
-app.use('/movies', moviesRouter)
+  app.use('/movies', createMovieRouter({ movieModel }))
 
-const PORT = process.env.port ?? 3000
+  const PORT = process.env.port ?? 3000
 
-app.listen(PORT, () => {
-  console.info(`Server listening on port http://localhost:${PORT}`)
-})
+  app.listen(PORT, () => {
+    console.info(`Server listening on port http://localhost:${PORT}`)
+  })
+}
--- controllers/movies.js
+++ controllers/movies.js
@@ -1,45 +1,47 @@
-// import { MovieModel } from '../models/local-fs/movie.js'
-import { MovieModel } from '../models/mysql/movie.js'
 import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
 
 export class MovieController {
-  static async getAll (req, res) {
+  constructor ({ movieModel }) {
+    this.movieModel = movieModel
+  }
+
+  getAll = async (req, res) => {
     const { genre } = req.query
-    const movies = await MovieModel.getAll({ genre })
+    const movies = await this.movieModel.getAll({ genre })
     res.json(movies)
   }
 
-  static async create (req, res) {
+  create = async (req, res) => {
     const result = validateMovie(req.body)
     if (result.error) {
       return res.status(400).send(result.error.message)
     }
-    const newMovie = await MovieModel.create(result.data)
+    const newMovie = await this.movieModel.create(result.data)
     res.status(201).json(newMovie)
   }
 
-  static async update (req, res) {
+  update = async (req, res) => {
     const result = validatePartialMovie(req.body)
     if (!result.success) {
       return res.status(400).send(result.error.message)
     }
     const { id } = req.params
-    const movieUpdated = await MovieModel.update({ id, input: result.data })
+    const movieUpdated = await this.movieModel.update({ id, input: result.data })
     if (!movieUpdated) return res.status(404).json({ error: 'Movie not found' })
     res.json(movieUpdated)
   }
 
-  static async delete (req, res) {
+  delete = async (req, res) => {
     const { id } = req.params
-    const wasDeleted = await MovieModel.delete({ id })
+    const wasDeleted = await this.movieModel.delete({ id })
     if (!wasDeleted) return res.status(404).json({ error: 'Movie not found' })
     res.json({ message: 'Movie deleted' })
   }
 
-  static async getById (req, res) {
+  getById = async (req, res) => {
     const { id } = req.params
     console.log(id)
-    const movie = await MovieModel.getById({ id })
+    const movie = await this.movieModel.getById({ id })
     if (movie) return res.json(movie)
     else res.status(404).send('<h1>Resource not found</h4>')
   }
